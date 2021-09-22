3f1d98802b8a511467835ed73b51954c1848e442
Anton Andreytsev <antonandreytsev@MacBook-Pro-Anton.local>
2021-09-22 12:07:49 +0300

    yandex recsys contest

---
+++ yandex_recsys/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project for recsys models for https://yandex.ru/cup/ml/',
+    author='Andreitsev Anton',
+    license='',
+)
